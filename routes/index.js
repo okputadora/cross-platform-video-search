dfba6cabd6671bd101d4b13c0c6ff439da0d8122
okputadora <okputadora@gmail.com>
2018-02-04 16:12:37 -0500

    setup basic layout with bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Cross-Platform Movie Search',
+    partials: {
+      header: "../views/partials/header"
+    }
+  });
 });
 
 module.exports = router;
